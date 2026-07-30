959bc5cb22c3f44bbf69e15a649db42c046ffad6
Anujchauhan-IN <chauhananuj1313@gmail.com>
2026-07-30 15:47:09 +0530

    16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function (people) {};
+const findTheOldest = function (people) {
+    let oldest = people[0];
+    const currentYear = new Date().getFullYear();
+
+    for (let i = 1; i < people.length; i++) {
+        const currentAge =
+            (people[i].yearOfDeath ?? currentYear) - people[i].yearOfBirth;
+        const oldestAge =
+            (oldest.yearOfDeath ?? currentYear) - oldest.yearOfBirth;
+        if (currentAge > oldestAge) {
+            oldest = people[i];
+        }
+    }
+    return oldest;
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
