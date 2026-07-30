8bed55d4cbb583da0d37bcd5be5cf9050c5ab744
Anujchauhan-IN <chauhananuj1313@gmail.com>
2026-07-30 20:13:49 +0530

    All exercises completed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = function (number) {
-    let result = [];
+    let fib = [0, 1];
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (typeof number === "string") {
+        number = Number(number);
+    }
+
+    for (let i = 2; i <= number; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[number];
 };
 
 // Do not edit below this line
