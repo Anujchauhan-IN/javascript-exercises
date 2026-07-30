41873a8d6efc1894dd08b7c44db9d7893bafb1eb
Anujchauhan-IN <chauhananuj1313@gmail.com>
2026-07-30 13:51:40 +0530

    add

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (array) {
+    let cleanArray = array.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversedArray = cleanArray.split("").reverse().join("");
+    if (reversedArray === cleanArray) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
